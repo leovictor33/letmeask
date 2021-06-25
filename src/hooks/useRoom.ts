ee8f9d9b61f0162808d613755e74197c1650619c
leonnnardo Araújo <leovictor33@gmail.com>
2021-06-25 17:13:42 -0300

    Aula 04 deletando pergunta;

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -25,7 +25,7 @@ type QuestionType = {
     isHighlighted: string;
     isAnswered: string;
     likeCount: number;
-    hasLiked: boolean;
+    likeId: string | undefined;
 }
 
 
@@ -48,7 +48,7 @@ export function useRoom(roomId: string) {
                     isHighlighted: value.isHighlighted,
                     isAnswered: value.isAnswered,
                     likeCount: Object.values(value.likes ?? {}).length,
-                    hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id)
+                    likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
                 }
             });
             setTitle(databaseRoom.title);
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -50,9 +50,9 @@ export function Room() {
         setNewQuestion('');
     }
 
-    async function handleLikeQuestion(questionId: string, hasLiked: boolean) {
-        if (hasLiked) {
-           //remover o like
+    async function handleLikeQuestion(questionId: string, likeId: string | undefined) {
+        if (likeId) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}/likes/${likeId}`).remove();
         } else {
             await database.ref(`rooms/${roomId}/questions/${questionId}/likes`).push({
                 authorId: user?.id,
@@ -104,10 +104,10 @@ export function Room() {
                                 author={question.author}
                             >
                                 <button
-                                    className={`like-button ${question.hasLiked ? 'liked' : ''}`}
+                                    className={`like-button ${question.likeId ? 'liked' : ''}`}
                                     type="button"
                                     aria-label="Marcar como gostei"
-                                    onClick={() => handleLikeQuestion(question.id, question.hasLiked)}
+                                    onClick={() => handleLikeQuestion(question.id, question.likeId)}
                                 >
                                     {question.likeCount > 0 && <span>{question.likeCount}</span>}
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
